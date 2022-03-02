9e139717b8e1683507f9b21da7e807ba9f833ea1
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2022-03-01 21:33:26 -0300

    Created atracoes route

---
+++ atracoes/api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from atracoes.models import Atracao 
+
+class Atracao_Serializer(ModelSerializer):
+  class Meta:
+    model = Atracao
+    fields = ('name','description','work_time','min_age')
--- atracoes/api/viewsets.py
+++ atracoes/api/viewsets.py
@@ -1,6 +1,7 @@
-# from rest_framework.viewsets import ModelViewSet
-# from atracoes.models import Atracao
+from rest_framework.viewsets import ModelViewSet
+from atracoes.models import Atracao
+from .serializers import Atracao_Serializer
 
-# class Atracoes_Viewse(ModelViewSet):
-#   queryset = Atracao.objects.all()
-#   # serializer_class = 
+class Atracoes_Viewse(ModelViewSet):
+  queryset = Atracao.objects.all()
+  serializer_class = Atracao_Serializer
