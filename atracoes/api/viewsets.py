61ed7d62cdb9566b3f5a5fcbe1ee554141800871
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2022-03-05 14:44:22 -0300

    Using django filter generics to create specific queries to search"

--- atracoes/api/viewsets.py
+++ atracoes/api/viewsets.py
@@ -1,7 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+# from rest_framework import filters
 from atracoes.models import Atracao
 from .serializers import Atracao_Serializer
 
 class Atracao_Viewset(ModelViewSet):
   queryset = Atracao.objects.all()
   serializer_class = Atracao_Serializer
+  filter_backends = [DjangoFilterBackend]
+  # filter_backends = [filters.SearchFilter]
+  # search_fields = ('name','description')
+  filter_fields = ('name','description')
--- comentarios/api/viewsets.py
+++ comentarios/api/viewsets.py
@@ -1,7 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
 from .serializers import Comentario_Serializer
 from comentarios.models import Comentario
 
 class Comentario_Viewset(ModelViewSet):
   queryset = Comentario.objects.all()
   serializer_class = Comentario_Serializer
+  filter_backends = [filters.SearchFilter]
+  search_fields = ['comentarios']
